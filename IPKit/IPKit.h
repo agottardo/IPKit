f6ce6fd5047d4ff50268648a49d2c818b77d1b2f
Andrea Gottardo <andrea@gottardo.me>
2018-12-18 12:27:15 -0800

    Initial Commit

---
+++ IPKit/IPKit.h
@@ -0,0 +1,19 @@
+//
+//  IPKit.h
+//  IPKit
+//
+//  Created by Andrea Gottardo on 2018-12-18.
+//  Copyright © 2018 Andrea Gottardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IPKit.
+FOUNDATION_EXPORT double IPKitVersionNumber;
+
+//! Project version string for IPKit.
+FOUNDATION_EXPORT const unsigned char IPKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IPKit/PublicHeader.h>
+
+
