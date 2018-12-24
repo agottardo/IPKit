51e3f6ca7c1a0b5f224a0c5b4ba1ba1f43cebfa8
Andrea Gottardo <andrea@gottardo.me>
2018-12-24 01:37:10 -0800

    Test forIP.

--- IPKitTests/IPKitTests.swift
+++ IPKitTests/IPKitTests.swift
@@ -42,10 +42,35 @@ class IPKitTests: XCTestCase {
         }
     }
     
+    func testGoogleFetch() {
+        let expect = expectation(description: "it should fetch Google DNS IP correctly")
+        IPAPI.shared.fetch(forIP: "8.8.8.8") { (response, error) in
+            if error != nil {
+                XCTFail("Network/parsing failure")
+            }
+            XCTAssert(response != nil, "Response should not be nil")
+            XCTAssert(response?.ip != nil, "The IP should not be nil")
+            XCTAssert((response?.ip?.count)! >= 7, "The IP should contain at least seven characters.")
+            XCTAssert(response?.location != nil, "The location should not be nil")
+            XCTAssertEqual(response?.location?.country, "US", "Country should be US")
+            XCTAssertEqual(response?.location?.city, "Mountain View", "City should be Mountain View")
+            XCTAssertTrue(response?.location?.isInEuropeanUnion == false, "California should not be in the 🇪🇺")
+            if response != nil && error == nil {
+                expect.fulfill()
+            }
+        }
+        waitForExpectations(timeout: IPAPI.shared.timeout) { error in
+            if error != nil {
+                XCTFail("It didn't fulfill.")
+            }
+            
+        }
+    }
+    
     func testInvalidIPs() {
         
-        let badIPs  = ["-1.255.255.255", "256.256.256.256", "a.b.c.d", "....", "0.a", "1.1.1", "...", ".....", "", "1234.168.0.1", ".1.1.1", "0.0..0"]
-        let goodIPs = ["192.168.0.1", "0.0.0.0", "255.255.255.255", "1.1.1.1"]
+        let badIPs  = ["-1.255.255.255", "256.256.256.256", "a.b.c.d", "....", "0.a", "1.1.1", "...", ".....", "", "1234.168.0.1", ".1.1.1", "0.0..0", "1a2.168.0.1", "a.168.0.1"]
+        let goodIPs = ["192.168.0.1", "0.0.0.0", "255.255.255.255", "1.1.1.1", "2.234.154.24"]
         
         for ip in badIPs {
             let expect = expectation(description: "it should not work with IP:" + ip)
